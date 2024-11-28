6860d0293a41aee7cbdc31ed9a9b9d4a17d8f9f5
anamez22 <ana.amezquita@correounivalle.edu.co>
2024-11-27 21:32:52 -0500

    Refactorización.py

--- Refactorización.py
+++ Refactorización.py
@@ -1,3 +1,4 @@
+#Refactorización 
 class Empleado:
     def __init__(self, nombre, apellido, salario_base):
         self.nombre = nombre
@@ -9,11 +10,15 @@ class Empleado:
 
 class Gerente(Empleado):
     def __init__(self, nombre, apellido, salario_base, bonus):
+#   se agrego super(). _init_ para inicializar los atributos 
+#   se agrego salario_base para que no haya repetición 
         super().__init__(nombre, apellido, salario_base)
         self.bonus = bonus
 
     def calcular_salario(self):
-        return super().calcular_salario() + self.bonus
+# se agrego super().calcular_salario para que calcule correctamente antes de agregar los componentes 
+        return super().calcular_salario() + self.bonus 
+
 
 class Vendedor(Empleado):
     def __init__(self, nombre, apellido, salario_base, comisiones):
